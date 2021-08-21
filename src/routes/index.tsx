22b915c597de7012465f94e3ad15b38fa623e15d
Samuel Levy <samuellevy.dev@gmail.com>
2021-08-21 03:40:07 -0300

    configure root import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
-import SplashScreen from '../pages/SplashScreen';
+import SplashScreen from '~/pages/SplashScreen';
 
 const Routes: React.FC = () => (
   <Switch>
