c47aeaffd5aca94b5ccbe08e0af014d567c59a80
Samuel Levy <samuellevy.dev@gmail.com>
2021-08-21 03:29:25 -0300

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-     <h1>Início</h1>
+      <h1>Início</h1>
     </div>
   );
-}
+};
 
 export default App;
